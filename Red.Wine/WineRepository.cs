ca28dfb281609e469201ec528339ea91b5506de4
Ronak Baldha <ronak.baldha@gmail.com>
2018-02-22 10:28:36 +0530

    Unflatten directory structure

--- Red.Wine/WineRepository.cs
+++ Red.Wine/WineRepository.cs
@@ -1,7 +1,6 @@
 ﻿using Red.Wine.Attributes;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
